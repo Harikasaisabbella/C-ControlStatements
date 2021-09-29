d991fd6103b4fcada8b2bff1c744d07f6abfbe9d
Harikasaisabbella <91045499+Harikasaisabbella@users.noreply.github.com>
2021-09-29 13:17:47 +0530

    Add files via upload

---
+++ 3 multiples.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{ int a, b;
+  printf("Enter two numbers\n");
+  scanf("%d%d", &a, &b);
+  while(a<=b)
+  {
+  	if(a%3!=0)
+  	{
+  		printf("%d ", a);
+	  }
+	  a++;
+  }
+}
