0335a0ea90ec378aa4d2c56ded253e05d2485f4e
Harikasaisabbella <91045499+Harikasaisabbella@users.noreply.github.com>
2021-10-18 12:12:31 +0530

    Add files via upload

---
+++ Factors of a number.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{ int n, i = 1;
+  printf("Enter a number: ");
+  scanf("%d", &n);
+  while(i <= n)
+  {
+  	if(n%i == 0)
+  	{
+	  
+  	printf("%d ", i);
+  }
+  i++;
+}
+
+
+}
