0dd4c0f2e759cfffa9a176c73e76138085a3efe0
Harikasaisabbella <91045499+Harikasaisabbella@users.noreply.github.com>
2021-10-18 12:39:38 +0530

    Add files via upload

---
+++ Multiplication table.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{ int a, b = 1;
+  printf("Enter a number: ");
+  scanf("%d", &a);
+  while(b <= 12)
+  {
+  	printf("%d * %d = %d\n\n", a, b, a*b);
+	  b++;
+
+  }
+  
+}
